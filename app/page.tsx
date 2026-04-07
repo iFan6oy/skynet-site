58c4fd276d6d999acf38c2c9b23a999103ff78a6
Jaylon Malone <malone.jaylon@gmail.com>
2026-04-06 19:59:38 -0500

    Add Telegram link to communication channels

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const CA = "AR6Sy3h6hzFxLjNFUNuE5YApgEci6gj1eAANU24cknAV";
 const PUMP_URL = `https://pump.fun/coin/${CA}`;
 const TWITTER_URL = "https://x.com/SOLSKYNETAI";
 const PAIR_ADDRESS = "AwHwzsQRhhagEttkHpp46QKJeRP71sAhzHg3aJNqPkc8";
+const TELEGRAM_URL = "https://t.me/SkynetSOL";
 const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
 
 export default function Home() {
@@ -141,6 +142,11 @@ export default function Home() {
             </SocialLink>
             <SocialLink href={DEXSCREENER_URL} label="DexScreener">
               <span className="text-lg">&#x1F4CA;</span>
+            </SocialLink>
+            <SocialLink href={TELEGRAM_URL} label="Telegram">
+              <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
+              </svg>
             </SocialLink>
           </div>
         </div>
